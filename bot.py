101ba882bab7290a73db07175652f63126cf3800
bias1x <giuliabiasone@hotmail.com>
2026-01-19 15:28:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,7 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 # Configurazione bacheche (ora monitoriamo anche il Prof. Siviero singolarmente)
 BACHECHE = [
     {"nome": "EPP", "url": "https://www.dei.unict.it/corsi/lm-56-epp/avvisi", "file": "pub_epp.txt", "emoji": "🔔"},
-    {"nome": "EPP (Docenti)", "url": "https://www.dei.unict.it/corsi/lm-56-epp/avvisi-docente", "file": "pub_epp_docenti.txt", "emoji": "👨‍🏫"},
-    {"nome": "Prof. Siviero", "url": "https://www.dei.unict.it/corsi/lm-56-epp/docenti/uid.dWtKQ08vZkRRVlJJaTNkUDdGY3U3WnJLam1SM2RrQS92R3NybUlHZUpUVT0=", "file": "pub_siviero.txt", "emoji": "📝"},
+    {"nome": "Prof. Siviero", "url": "https://www.dei.unict.it/corsi/lm-56-epp/docenti/uid.dWtKQ08vZkRRVlJJaTNkUDdGY3U3WnJLam1SM2RrQS92R3NybUlHZUpUVT0=#TAB4", "file": "pub_siviero.txt", "emoji": "📝"},
     {"nome": "DEI", "url": "https://www.dei.unict.it/Comunicazioni/elenco-news", "file": "pub_dei.txt", "emoji": "🏛️"},
     {"nome": "UNICT", "url": "https://www.unict.it/it/ateneo/news", "file": "pub_unict.txt", "emoji": "🌐"}
 ]
